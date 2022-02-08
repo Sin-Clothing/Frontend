0bdbf91b7ee50c4cf1b002c919cb16ce402a3196
SeOneAndOnly <71444626+SeOneAndOnly@users.noreply.github.com>
2022-02-08 13:00:18 +0100

    Cart css updated

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,71 +1,80 @@
 import React, { useState } from "react";
+import "./Cart.css";
 
 export default function Cart(props) {
-  const { cartItems, onAddItemToCart, onRemoveItemFromCart, currentSize } = props;
+  const { product ,cartItems, onAddItemToCart, onRemoveItemFromCart, currentSize } = props;
   const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
   const taxPrice = itemsPrice * 0.14;
   const shippingPrice = itemsPrice > 2000 ? 0 : 20;
   const totalPrice = itemsPrice + taxPrice + shippingPrice;
   return (
-    <div className="block col-1">
-      <h2>Cart Items</h2>
-      <div>
-        {cartItems.length === 0 && <div>Cart is empty</div>}
-        {cartItems.map((item) => (
-          <div key={item.id} className="row">
-            <div className="col-2">{item.name} {item.size.name}</div>
-            
-            <div className="col-2">
-              <button onClick={() => onRemoveItemFromCart(item)} className="remove">
-                -
-              </button>{' '}
-              <button onClick={() => onAddItemToCart(item)} className="add">
-                +
-              </button>
-            </div>
-
-            <div className="col-2 text-right">
-              {item.qty} x ${item.price.toFixed(2)}
-            </div>
+    <body>
+      <div className="Cart-container">
+        <div className="Header">
+          {cartItems != 0 && <h3 className="Heading">Cart&nbsp;Items</h3>}
           </div>
-        ))}
+          <div>
+            {cartItems.length === 0 && <div className="empty">Cart is empty</div>}
+            
+            {cartItems.map((item) => (
+              <div key={item.id} className="Cart-Items">
+                <div className="image-box"><img src={item.pictureUrl} height={140} alt="product" /></div>
+                <div className="about">
+                  <h2 className="title"> {item.name}</h2> <h3 className="subtitle">{item.size.name}</h3>
+                </div>
+                <div className="counter">
+                  <button onClick={() => onRemoveItemFromCart(item)} className="btn">
+                    -
+                  </button>
+                  <div className="count">{item.qty}</div>
+                  <button onClick={() => onAddItemToCart(item)} className="btn">
+                    +
+                  </button>
+                </div>
 
-        {cartItems.length !== 0 && (
-          <>
-            <hr></hr>
-            <div className="row">
-              <div className="col-2">Items Price</div>
-              <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
-            </div>
-            <div className="row">
-              <div className="col-2">Tax Price</div>
-              <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
-            </div>
-            <div className="row">
-              <div className="col-2">Shipping Price</div>
-              <div className="col-1 text-right">
-                ${shippingPrice.toFixed(2)}
+                <div className="prices">
+                  ${item.price.toFixed(2)}
+                </div>
               </div>
-            </div>
+            ))}
 
-            <div className="row">
-              <div className="col-2">
-                <strong>Total Price</strong>
-              </div>
-              <div className="col-1 text-right">
-                <strong>${totalPrice.toFixed(2)}</strong>
-              </div>
-            </div>
-            <hr />
-            <div className="row">
-              <button onClick={() => alert('Implement Checkout!')}>
-                Checkout
-              </button>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
+            {cartItems.length !== 0 && (
+              <>
+                <hr></hr>
+                <div>
+                  <div className="ueber">Items Price</div>
+                  <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
+                </div>
+                <div>
+                  <div className="ueber">Tax Price</div>
+                  <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
+                </div>
+                <div>
+                  <div className="ueber">Shipping Price</div>
+                  <div className="col-1 text-right">
+                    ${shippingPrice.toFixed(2)}
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="total-amount">
+                    <strong>Total Price</strong>
+                  </div>
+                  <div className="col-1 text-right">
+                    <strong>${totalPrice.toFixed(2)}</strong>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <button className="button" onClick={() => alert('Implement Checkout!')}>
+                    Checkout
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+        </body>  
   );
 
   function Zoom({ src }) {
